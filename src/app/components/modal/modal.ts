970730225d10e44cf1603c0473692317deb6645b
Sminotti <sebastianminotti@gmail.com>
2025-07-27 23:09:58 -0300

    modales de sign-in y sign-up ok

--- src/app/components/modal/modal.ts
+++ src/app/components/modal/modal.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { SignIn } from '../forms/sign-in/sign-in';
+import { SignUp } from '../forms/sign-up/sign-up';
+import { MatDialogModule } from '@angular/material/dialog';
+import { NgIf, CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-modal',
-  imports: [],
-  templateUrl: './modal.html',
-  styles: ``
+  standalone: true,
+  imports: [
+    MatDialogModule,
+    SignIn,
+    SignUp,
+    NgIf,
+    CommonModule
+  ],
+  templateUrl: './modal.html'
 })
-export class Modal {
-
+export class ModalComponent {
+  formType = signal<'login' | 'register'>('login');
 }
--- src/app/components/toolbar/toolbar.ts
+++ src/app/components/toolbar/toolbar.ts
@@ -4,6 +4,9 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterModule } from '@angular/router';
 import { MatSidenav } from '@angular/material/sidenav';
+import { MatDialog } from '@angular/material/dialog';
+
+import { ModalComponent } from '../modal/modal';
 
 @Component({
   selector: 'app-toolbar',
@@ -21,4 +24,11 @@ import { MatSidenav } from '@angular/material/sidenav';
 export class Toolbar {
   @Input() drawer!: MatSidenav;
   // readonly drawer = input.required<MatSidenav>();
+
+  constructor(private dialog: MatDialog) {}
+
+  openModal(type: 'login' | 'register') {
+    const dialogRef = this.dialog.open(ModalComponent);
+    dialogRef.componentInstance.formType.set(type);
+  }
 }
